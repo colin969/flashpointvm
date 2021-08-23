c25754da550224e80da516d61403214263f42adb
Colin Berry <colinb969@gmail.com>
2021-08-23 17:20:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-: ${ALPINE_MAKEVM:="https://raw.githubusercontent.com/alpinelinux/alpine-make-vm-image/v0.7.0/alpine-make-vm-image"}
 if [ "$(id -u)" -ne "0" ]; then
 	echo "Please run as root"
 	exit 1
@@ -9,7 +8,7 @@ if ! command -v qemu-img >/dev/null; then
 	exit 1
 fi
 tmp=$(mktemp -u -t alpine.XXXXXX)
-curl "$ALPINE_MAKEVM" | sh /dev/stdin -f qcow2 -c "$tmp" setup.sh \
+sh alpine-make-vm-image -f qcow2 -c "$tmp" setup.sh \
 && echo Shrinking image, please wait \
 && qemu-img convert -O qcow2 "$tmp" "$1" \
 && [ $SUDO_USER ] && chown "$SUDO_USER": "$1"
